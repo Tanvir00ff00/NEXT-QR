29149fbee23a7351400dc69c37f978cf3d3205dd
Tanvir Hossain <tanvir.00ff00@gmail.com>
2025-12-29 16:35:26 +0600

    Updates

---
+++ _next/static/xuffLhtKH1GxUqcHVQfYI/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NEXT-QR//_next/:path+",
+        "destination": "/NEXT-QR/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
